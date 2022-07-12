d5359af63e16db5707bf565164e8770f09528646
BlueRobin12345 <107601768+BlueRobin12345@users.noreply.github.com>
2022-07-12 08:42:31 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 def 右转():
-    neZha.set_motor_speed(neZha.MotorList.M1, 33)
-    neZha.set_motor_speed(neZha.MotorList.M2, -33)
+    neZha.set_motor_speed(neZha.MotorList.M1, 35)
+    neZha.set_motor_speed(neZha.MotorList.M2, -35)
     basic.pause(400)
     neZha.stop_all_motor()
 def 左转():
-    neZha.set_motor_speed(neZha.MotorList.M1, -33)
-    neZha.set_motor_speed(neZha.MotorList.M2, 33)
+    neZha.set_motor_speed(neZha.MotorList.M1, -35)
+    neZha.set_motor_speed(neZha.MotorList.M2, 35)
     basic.pause(400)
     neZha.stop_all_motor()
 
 def on_button_pressed_a():
     basic.show_string("1")
-    到路口1()
+    到路口()
     左转()
-    neZha.set_motor_speed(neZha.MotorList.M2, 50)
-    neZha.set_motor_speed(neZha.MotorList.M1, 50)
-    basic.pause(200)
-    neZha.stop_all_motor()
+    后退()
     识别颜色()
     basic.pause(500)
-    neZha.set_motor_speed(neZha.MotorList.M2, -50)
-    neZha.set_motor_speed(neZha.MotorList.M1, -50)
-    basic.pause(200)
-    neZha.stop_all_motor()
+    前进()
     右转()
     basic.pause(50)
+    前进()
     到路口2()
     basic.pause(50)
     右转()
     basic.pause(50)
-    到路口1()
+    到路口()
     basic.pause(50)
     舵机()
     basic.pause(300)
@@ -39,22 +34,8 @@ def on_button_pressed_a():
     basic.pause(700)
     neZha.stop_all_motor()
     左转()
-    neZha.set_motor_speed(neZha.MotorList.M2, -50)
-    neZha.set_motor_speed(neZha.MotorList.M1, -50)
-    basic.pause(2000)
-    neZha.stop_all_motor()
-    到路口1()
-    右转()
-    neZha.set_motor_speed(neZha.MotorList.M2, -50)
-    neZha.set_motor_speed(neZha.MotorList.M1, -50)
-    basic.pause(300)
-    neZha.stop_all_motor()
-    basic.pause(50)
-    neZha.set_motor_speed(neZha.MotorList.M2, 50)
-    neZha.set_motor_speed(neZha.MotorList.M1, 50)
-    basic.pause(300)
-    neZha.stop_all_motor()
-    
+    basic.pause(100)
+    到路口()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def 巡线():
@@ -63,24 +44,41 @@ def 巡线():
         neZha.set_motor_speed(neZha.MotorList.M1, -40)
         neZha.set_motor_speed(neZha.MotorList.M2, -40)
     if PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_3) or PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_11):
-        neZha.set_motor_speed(neZha.MotorList.M1, -80)
-        neZha.set_motor_speed(neZha.MotorList.M2, -9)
+        neZha.set_motor_speed(neZha.MotorList.M1, -60)
+        neZha.set_motor_speed(neZha.MotorList.M2, -5)
     if PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_2) or PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_14):
-        neZha.set_motor_speed(neZha.MotorList.M1, -9)
-        neZha.set_motor_speed(neZha.MotorList.M2, -80)
+        neZha.set_motor_speed(neZha.MotorList.M1, -5)
+        neZha.set_motor_speed(neZha.MotorList.M2, -60)
     if PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_8):
-        neZha.set_motor_speed(neZha.MotorList.M1, -80)
+        neZha.set_motor_speed(neZha.MotorList.M1, -60)
         neZha.set_motor_speed(neZha.MotorList.M2, -4)
     if PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_12):
         neZha.set_motor_speed(neZha.MotorList.M1, -4)
-        neZha.set_motor_speed(neZha.MotorList.M2, -80)
+        neZha.set_motor_speed(neZha.MotorList.M2, -60)
     if PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_0):
         neZha.set_motor_speed(neZha.MotorList.M1, -40)
         neZha.set_motor_speed(neZha.MotorList.M2, -40)
+def 后退():
+    neZha.set_motor_speed(neZha.MotorList.M2, 50)
+    neZha.set_motor_speed(neZha.MotorList.M1, 50)
+    basic.pause(200)
+    neZha.stop_all_motor()
 def 舵机():
     neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, 120)
     basic.pause(300)
     neZha.set_servo_angel(neZha.ServoTypeList._360, neZha.ServoList.S1, 225)
+def 前进():
+    neZha.set_motor_speed(neZha.MotorList.M2, -50)
+    neZha.set_motor_speed(neZha.MotorList.M1, -50)
+    basic.pause(200)
+    neZha.stop_all_motor()
+def 到路口():
+    while not (PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_5)):
+        巡线()
+    neZha.set_motor_speed(neZha.MotorList.M2, -50)
+    neZha.set_motor_speed(neZha.MotorList.M1, -50)
+    basic.pause(200)
+    neZha.stop_all_motor()
 def 识别颜色():
     while True:
         if PlanetX_Basic.check_color(PlanetX_Basic.ColorList.RED):
@@ -98,20 +96,6 @@ def 识别颜色():
         else:
             neZha.set_motor_speed(neZha.MotorList.M1, 50)
             neZha.set_motor_speed(neZha.MotorList.M2, 50)
-
-def on_button_pressed_b():
-    左转()
-    basic.pause(1000)
-    右转()
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def 到路口1():
-    while not (PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_5)):
-        巡线()
-    neZha.set_motor_speed(neZha.MotorList.M2, -50)
-    neZha.set_motor_speed(neZha.MotorList.M1, -50)
-    basic.pause(200)
-    neZha.stop_all_motor()
 def 到路口2():
     while not (PlanetX_Basic.trackbit_state(PlanetX_Basic.TrackbitStateType.TRACKING_STATE_13)):
         巡线()
--- main.ts
+++ main.ts
@@ -1,36 +1,33 @@
-function 右转 () {
+function 右转() {
     neZha.setMotorSpeed(neZha.MotorList.M1, 35)
     neZha.setMotorSpeed(neZha.MotorList.M2, -35)
     basic.pause(400)
     neZha.stopAllMotor()
 }
-function 左转 () {
+
+function 左转() {
     neZha.setMotorSpeed(neZha.MotorList.M1, -35)
     neZha.setMotorSpeed(neZha.MotorList.M2, 35)
     basic.pause(400)
     neZha.stopAllMotor()
 }
-input.onButtonPressed(Button.A, function () {
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showString("1")
-    到路口1()
+    到路口()
     左转()
-    neZha.setMotorSpeed(neZha.MotorList.M2, 50)
-    neZha.setMotorSpeed(neZha.MotorList.M1, 50)
-    basic.pause(200)
-    neZha.stopAllMotor()
+    后退()
     识别颜色()
     basic.pause(500)
-    neZha.setMotorSpeed(neZha.MotorList.M2, -50)
-    neZha.setMotorSpeed(neZha.MotorList.M1, -50)
-    basic.pause(200)
-    neZha.stopAllMotor()
+    前进()
     右转()
     basic.pause(50)
+    前进()
     到路口2()
     basic.pause(50)
     右转()
     basic.pause(50)
-    到路口1()
+    到路口()
     basic.pause(50)
     舵机()
     basic.pause(300)
@@ -41,62 +38,96 @@ input.onButtonPressed(Button.A, function () {
     neZha.stopAllMotor()
     左转()
     basic.pause(100)
-    到路口1()
+    到路口()
 })
-function 巡线 () {
+function 巡线() {
     PlanetX_Basic.Trackbit_get_state_value()
     if (PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_1)) {
         neZha.setMotorSpeed(neZha.MotorList.M1, -40)
         neZha.setMotorSpeed(neZha.MotorList.M2, -40)
     }
+    
     if (PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_3) || PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_11)) {
-        neZha.setMotorSpeed(neZha.MotorList.M1, -80)
-        neZha.setMotorSpeed(neZha.MotorList.M2, -9)
+        neZha.setMotorSpeed(neZha.MotorList.M1, -60)
+        neZha.setMotorSpeed(neZha.MotorList.M2, -5)
     }
+    
     if (PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_2) || PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_14)) {
-        neZha.setMotorSpeed(neZha.MotorList.M1, -9)
-        neZha.setMotorSpeed(neZha.MotorList.M2, -80)
+        neZha.setMotorSpeed(neZha.MotorList.M1, -5)
+        neZha.setMotorSpeed(neZha.MotorList.M2, -60)
     }
+    
     if (PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_8)) {
-        neZha.setMotorSpeed(neZha.MotorList.M1, -80)
+        neZha.setMotorSpeed(neZha.MotorList.M1, -60)
         neZha.setMotorSpeed(neZha.MotorList.M2, -4)
     }
+    
     if (PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_12)) {
         neZha.setMotorSpeed(neZha.MotorList.M1, -4)
-        neZha.setMotorSpeed(neZha.MotorList.M2, -80)
+        neZha.setMotorSpeed(neZha.MotorList.M2, -60)
     }
+    
     if (PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_0)) {
         neZha.setMotorSpeed(neZha.MotorList.M1, -40)
         neZha.setMotorSpeed(neZha.MotorList.M2, -40)
     }
+    
 }
-function 舵机 () {
+
+function 后退() {
+    neZha.setMotorSpeed(neZha.MotorList.M2, 50)
+    neZha.setMotorSpeed(neZha.MotorList.M1, 50)
+    basic.pause(200)
+    neZha.stopAllMotor()
+}
+
+function 舵机() {
     neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 120)
     basic.pause(300)
     neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 225)
 }
-function 识别颜色 () {
+
+function 前进() {
+    neZha.setMotorSpeed(neZha.MotorList.M2, -50)
+    neZha.setMotorSpeed(neZha.MotorList.M1, -50)
+    basic.pause(200)
+    neZha.stopAllMotor()
+}
+
+function 到路口() {
+    while (!PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_5)) {
+        巡线()
+    }
+    neZha.setMotorSpeed(neZha.MotorList.M2, -50)
+    neZha.setMotorSpeed(neZha.MotorList.M1, -50)
+    basic.pause(200)
+    neZha.stopAllMotor()
+}
+
+function 识别颜色() {
     while (true) {
         if (PlanetX_Basic.checkColor(PlanetX_Basic.ColorList.red)) {
             PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J1, true)
             neZha.stopAllMotor()
-            break;
+            break
         } else if (PlanetX_Basic.checkColor(PlanetX_Basic.ColorList.yellow)) {
             PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J2, true)
             neZha.stopAllMotor()
-            break;
+            break
         } else if (PlanetX_Basic.checkColor(PlanetX_Basic.ColorList.green)) {
             PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, true)
             neZha.stopAllMotor()
-            break;
+            break
         } else {
             neZha.setMotorSpeed(neZha.MotorList.M1, 50)
             neZha.setMotorSpeed(neZha.MotorList.M2, 50)
         }
+        
     }
 }
-function 到路口1 () {
-    while (!(PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_5))) {
+
+function 到路口2() {
+    while (!PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_13)) {
         巡线()
     }
     neZha.setMotorSpeed(neZha.MotorList.M2, -50)
@@ -104,13 +135,5 @@ function 到路口1 () {
     basic.pause(200)
     neZha.stopAllMotor()
 }
-function 到路口2 () {
-    while (!(PlanetX_Basic.TrackbitState(PlanetX_Basic.TrackbitStateType.Tracking_State_13))) {
-        巡线()
-    }
-    neZha.setMotorSpeed(neZha.MotorList.M2, -50)
-    neZha.setMotorSpeed(neZha.MotorList.M1, -50)
-    basic.pause(200)
-    neZha.stopAllMotor()
-}
+
 neZha.setServoAngel(neZha.ServoTypeList._360, neZha.ServoList.S1, 225)
